73bf31b27377721d960666921d744f26b74cfc76
wojtek <wojtekkulma@gmail.com>
2015-10-30 14:16:30 +0100

    setup

---
+++ ops/src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('ops', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ui.router', 'mm.foundation', 'toastr']);
+
+})();
