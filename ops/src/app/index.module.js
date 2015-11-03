fb79ffcc0ffb37ff5b08076fc87ccc9d21a1937d
wojtek.kulma@gmail.com <wojtek.kulma@polidea.com>
2015-11-03 18:09:50 +0100

    homepage

--- ops/src/app/index.module.js
+++
@@ -1,7 +0,0 @@
-(function() {
-  'use strict';
-
-  angular
-    .module('ops', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ui.router', 'mm.foundation', 'toastr']);
-
-})();
